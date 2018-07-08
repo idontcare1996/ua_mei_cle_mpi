7608f7333b53ba006f1981467911fe70a32e7d49
Carlos Oliveira <cmbo1996@gmail.com>
2018-07-08 11:57:53 +0100

    Initial Commit

---
+++ rmconf.c
@@ -0,0 +1,25 @@
+/*
+
+TASK 1 - Creation of room configuration data files
+
+*/
+
+// IMPORTS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <math.h>
+
+// DEFINE FUNCTIONS FOR LATER USE
+
+float node_spacing (float c, int fu);
+
+
+int main ()
+{
+    // VARIABLES TO USE
+
+    return 0;
+}
